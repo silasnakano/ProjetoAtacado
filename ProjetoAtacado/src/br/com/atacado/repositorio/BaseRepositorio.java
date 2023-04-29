4b48e0800ee4c6d9563a96600b0dba3bb3b974e1
silasnakano <ryobyro@gmail.com>
2023-04-28 20:55:32 -0400

    Implementação dos arquivos de repositorio

---
+++ ProjetoAtacado/src/br/com/atacado/repositorio/BaseRepositorio.java
@@ -0,0 +1,7 @@
+package br.com.atacado.repositorio;
+
+public abstract class BaseRepositorio<TTipo> {
+
+    // CRUD
+
+}
---
+++ ProjetoAtacado/src/br/com/atacado/repositorio/CategoriaRepositorio.java
@@ -0,0 +1,39 @@
+package br.com.atacado.repositorio;
+
+import java.util.List;
+
+import br.com.atacado.dominio.Categoria;
+
+public class CategoriaRepositorio implements IBaseRepositorio<Categoria> {
+
+    @Override
+    public Categoria Create(Categoria obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Create'");
+    }
+
+    @Override
+    public Categoria Read(int chave) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Read'");
+    }
+
+    @Override
+    public List<Categoria> Read() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Read'");
+    }
+
+    @Override
+    public Categoria Update(Categoria obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Update'");
+    }
+
+    @Override
+    public Categoria Delete(int chave) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Delete'");
+    }
+
+}
